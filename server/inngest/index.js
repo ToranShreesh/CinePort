0f644951ee24bcf480f4cff7f323ec94c9b7bea8
Toran Shreesh <nabinshreesh195@gmail.com>
2026-06-06 14:46:40 +0545

    Completed movie booking application and project testing

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -106,14 +106,14 @@ const sendBookingConfirmationEmail = inngest.createFunction(
 const sendShowReminders = inngest.createFunction(
     { 
         id: "send-show-reminders",
-        cron: "0 */8 * * *"   // Every 8 hours
+       triggers: [{ cron: "0 */8 * * *" }]
     },
     async ({ step }) => {
         const now = new Date();
         const in8Hours = new Date(now.getTime() + 8 * 60 * 60 * 1000);
         const windowStart = new Date(in8Hours.getTime() - 10 * 60 * 1000);
 
-        const remainderTasks = await step.run("prepare-reminder-tasks", async () => {
+        const reminderTasks = await step.run("prepare-reminder-tasks", async () => {
             const shows = await Show.find({
                 showTime: { $gte: windowStart, $lte: in8Hours }   // ← Use correct field name
             }).populate('movie');
